4bae6aa25b5f488bba16ca7226110b93dea66f31
command293 <166911166+ever39@users.noreply.github.com>
2024-05-13 10:16:15 +0300

    new files
    much easier to read! hooray

---
+++ localFlashlight/Options.cs
@@ -0,0 +1,33 @@
+﻿public enum BatteryDisplayOptions
+{
+    Disabled,
+    Bar,
+    Percentage,
+    VerticalBar,
+    CircularBar,
+    All
+}
+
+public enum TextDisplayOptions
+{
+    Percent,
+    AccuratePercent,
+    Time,
+    All
+}
+
+public enum SoundOptions
+{
+    OriginalLightSounds,
+    InGameFlashlight,
+    OtherwordlyLight
+}
+
+public enum RechargeOptions
+{
+    Time,
+    Shake,
+    Dynamo,
+    OnShipEnter,
+    Apparatice
+}
---
+++ localFlashlight/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using localFlashlight;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("localFlashlight")]
+[assembly: AssemblyDescription("lethal company mod based off of a random idea i had :)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("localFlashlight")]
+[assembly: AssemblyCopyright("Copyright © ever39 2024")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1170dd0f-aa92-4f52-b072-435b0241aa79")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can OriginalLightSounds the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(Plugin.VERSION)]
+[assembly: AssemblyFileVersion(Plugin.VERSION)]
