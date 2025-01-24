ab407b6e03038050e41b2d55c23442f8a5a4f432
command293 <command9874@protonmail.com>
2025-01-24 22:29:14 +0200

    new code

---
+++ LocalFlashlight/Options.cs
@@ -0,0 +1,39 @@
+﻿public enum BatteryDisplayOptions
+{
+    Disabled,
+    Bar,
+    Text,
+    VerticalBar,
+    CircularBar,
+    All
+}
+
+public enum TextDisplayOptions
+{
+    None,
+    Percent,
+    AccuratePercent,
+    Time,
+    All
+}
+
+///maybe just keep the in game flashlight and customsounds options?
+public enum SoundOptions
+{
+    InGameFlashlight,
+    CustomSounds
+}
+
+public enum RechargeOptions
+{
+    Time,
+    Shake,
+    HandCranked,
+    FacilityPowered
+}
+
+public enum GlobalFlashlightOptions
+{
+    CrewUpgrade,
+    Unrestricted
+}
